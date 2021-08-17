198a98a72083ecfd9c284eeccc160d3602555e16
Shayan Guha <shayanguha@gmail.com>
2021-08-17 13:06:25 -0700

    fix: URL for Expo Go projects

--- src/url.ts
+++ src/url.ts
@@ -1,13 +1,13 @@
 import { ProjectFlavor } from './config';
 
-const generateQRCodeBaseURLForGo = 'https://api.qrserver.com/v1/create-qr-code/?size=512x512&data=exp://exp.host';
+const generateQRCodeBaseURLForGo = 'https://api.qrserver.com/v1/create-qr-code';
 const generateQRCodeBaseURLForBare = 'https://us-central1-exponentjs.cloudfunctions.net';
 
 export function createQRCodeURL(projectFlavor: ProjectFlavor, manifestURL: string, scheme: string): string {
 	if (projectFlavor === ProjectFlavor.DevelopmentClient) {
 		return `${generateQRCodeBaseURLForBare}/generateQRCode/development-client?appScheme=${scheme}&url=${manifestURL}`;
 	} else if (projectFlavor === ProjectFlavor.ExpoGo) {
-		return `${generateQRCodeBaseURLForGo}/${manifestURL}`;
+		return `${generateQRCodeBaseURLForGo}?size=512x512&data=${manifestURL.replace('https://', 'exp://')}`;
 	}
 
 	throw new Error('Unknown project flavor.');
