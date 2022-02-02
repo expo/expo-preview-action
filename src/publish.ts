b8b5173d4c8b2267e1265baf0bd9322abf776fa8
maker-mark <markgoldstein12@gmail.com>
2022-02-02 17:53:20 -0500

    Adds findProjectPage & updates publish to return object with manifestURL and projectPageURL

--- src/publish.ts
+++ src/publish.ts
@@ -1,6 +1,5 @@
-import { getExecOutput } from '@actions/exec';
-
-import { PublishConfig } from './config';
+import { getExecOutput } from "@actions/exec"
+import { PublishConfig } from "./config"
 
 export function findManifestUrl(text: string): string {
 	const regex = /📝 {2}Manifest: ([^ ]*)/;
@@ -12,15 +11,28 @@ export function findManifestUrl(text: string): string {
 	return match;
 }
 
-export async function publish(config: PublishConfig): Promise<string> {
+export function findProjectPage(text: string): string {
+	const regex = /⚙️ {3}Project page: ([^ ]*)/;
+
+	const match = (text.match(regex) || [])[1];
+	if (!match) {
+		throw new Error("Couldn't extract project page URL.");
+	}
+
+	return match;
+}
+
+export async function publish(config: PublishConfig): Promise<{ manifestURL: string; projectPageURL: string }> {
 	const { exitCode, stderr, stdout } = await getExecOutput(
 		`${config.cliPath} publish --release-channel=${config.channel}`,
 		undefined,
-		{ cwd: config.projectRoot },
+		{ cwd: config.projectRoot }
 	);
 	if (exitCode !== 0) {
 		throw new Error(stderr);
 	}
+	const manifestURL = findManifestUrl(stdout);
+	const projectPageURL = findProjectPage(stdout);
 
-	return findManifestUrl(stdout);
+	return { manifestURL, projectPageURL };
 }
